dd39ab5a5d4be6b4d96078f62e6b030bc8f66931
Carlos Henrique Chaves Barreira <139780813+CarlosChaves2023@users.noreply.github.com>
2023-10-29 11:26:55 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,25 @@
-'''Deu problema no primeiro , estamos fazendo um novo.'''
+- verificar se gitbash já está baixado
+
+caso nao esteja:
+- entra no site do git e baixa para windows
+
+para clonar repositório do giithub para o git:
+- botão dirito na área de trabalho
+- mais opções
+- gitbash here
+- gitclone -> copia o link do repositório onde tem o 'code'
+- shift + insert
+
+No VScode:
+- file
+- open folder
+- abre pasta
+- canto inferior esquerdo aparece o simbolo da ramificaçao
+- git status para ver se tem arquivo
+- git add . (adiciona tudo)
+- git add . nomedoarquivo.extensão - add só o arquivo desse nome e extrensão
+- git commit -m "nomear o commit entre aspas"
+
+Git pergunta quem sou:
+- git config --global user.name"meu usuario github"
+- git config --global user.email ---email github
